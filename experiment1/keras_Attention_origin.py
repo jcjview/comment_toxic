091981143324e620b07b974317acbe4767251090
chongjiu <chongjiu.jin@tinno.com>
2018-01-11 16:39:11 +0800

    add cang code 0.043

--- experiment1/keras_Attention_origin.py
+++ experiment1/keras_Attention_origin.py
@@ -11,41 +11,27 @@ referrence Code:https://www.kaggle.com/lystdo/lstm-with-word2vec-embeddings
 ########################################
 ## import packages
 ########################################
-import os
 import re
-import csv
-import codecs
+
 import numpy as np
 import pandas as pd
-
+from keras import backend as K
+# from keras import initializations
+from keras import initializers, regularizers, constraints
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.engine.topology import Layer
+from keras.layers import Dense, Input, LSTM, Embedding, Dropout
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
-from string import punctuation
 
-from gensim.models import KeyedVectors
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation
-from keras.layers.merge import concatenate
-from keras.models import Model
-from keras.layers.normalization import BatchNormalization
-from keras.callbacks import EarlyStopping, ModelCheckpoint
-
-
-
-
-import sys
 
 ########################################
 ## set directories and parameters
 ########################################
-
-
-
-from keras import backend as K
-from keras.engine.topology import Layer
-#from keras import initializations
-from keras import initializers, regularizers, constraints
 
 
 class Attention(Layer):
@@ -69,7 +55,7 @@ class Attention(Layer):
             model.add(Attention())
         """
         self.supports_masking = True
-        #self.init = initializations.get('glorot_uniform')
+        # self.init = initializations.get('glorot_uniform')
         self.init = initializers.get('glorot_uniform')
 
         self.W_regularizer = regularizers.get(W_regularizer)
@@ -136,17 +122,20 @@ class Attention(Layer):
 
         a = K.expand_dims(a)
         weighted_input = x * a
-    #print weigthted_input.shape
+        # print weigthted_input.shape
         return K.sum(weighted_input, axis=1)
 
     def compute_output_shape(self, input_shape):
-        #return input_shape[0], input_shape[-1]
-        return input_shape[0],  self.features_dim
-        
-path = '../input/'
-EMBEDDING_FILE=path+'glove.840B.300d.txt'
-TRAIN_DATA_FILE=path+'train.csv'
-TEST_DATA_FILE=path+'test.csv'
+        # return input_shape[0], input_shape[-1]
+        return input_shape[0], self.features_dim
+
+
+import config
+
+path = '../data/'
+EMBEDDING_FILE = config.GLOVE_EMBEDDING_FILE
+TRAIN_DATA_FILE = config.TRAIN_FILE
+TEST_DATA_FILE = config.TRAIN_VALID_FILE
 
 MAX_SEQUENCE_LENGTH = 150
 MAX_NB_WORDS = 100000
@@ -165,13 +154,13 @@ act = 'relu'
 ########################################
 print('Indexing word vectors')
 
-#Glove Vectors
+# Glove Vectors
 embeddings_index = {}
 f = open(EMBEDDING_FILE)
 for line in f:
     values = line.split()
-    word = values[0]
-    coefs = np.asarray(values[1:], dtype='float32')
+    word = ' '.join(values[:-300])
+    coefs = np.asarray(values[-300:], dtype='float32')
     embeddings_index[word] = coefs
 f.close()
 
@@ -185,15 +174,16 @@ test_df = pd.read_csv(TEST_DATA_FILE)
 ########################################
 print('Processing text dataset')
 
-#Regex to remove all Non-Alpha Numeric and space
-special_character_removal=re.compile(r'[^a-z\d ]',re.IGNORECASE)
+# Regex to remove all Non-Alpha Numeric and space
+special_character_removal = re.compile(r'[^a-z\d ]', re.IGNORECASE)
 
-#regex to replace all numerics
-replace_numbers=re.compile(r'\d+',re.IGNORECASE)
+# regex to replace all numerics
+replace_numbers = re.compile(r'\d+', re.IGNORECASE)
+
 
 def text_to_wordlist(text, remove_stopwords=False, stem_words=False):
     # Clean the text, with the option to remove stopwords and to stem words.
-    
+
     # Convert words to lower case and split them
     text = text.lower().split()
 
@@ -201,14 +191,14 @@ def text_to_wordlist(text, remove_stopwords=False, stem_words=False):
     if remove_stopwords:
         stops = set(stopwords.words("english"))
         text = [w for w in text if not w in stops]
-    
+
     text = " ".join(text)
-    
-    #Remove Special Characters
-    text=special_character_removal.sub('',text)
-    
-    #Replace Numbers
-    text=replace_numbers.sub('n',text)
+
+    # Remove Special Characters
+    text = special_character_removal.sub('', text)
+
+    # Replace Numbers
+    text = replace_numbers.sub('n', text)
 
     # Optionally, shorten words to their stems
     if stem_words:
@@ -216,9 +206,9 @@ def text_to_wordlist(text, remove_stopwords=False, stem_words=False):
         stemmer = SnowballStemmer('english')
         stemmed_words = [stemmer.stem(word) for word in text]
         text = " ".join(stemmed_words)
-    
+
     # Return a list of words
-    return(text)
+    return (text)
 
 
 list_sentences_train = train_df["comment_text"].fillna("NA").values
@@ -226,12 +216,11 @@ list_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identit
 y = train_df[list_classes].values
 list_sentences_test = test_df["comment_text"].fillna("NA").values
 
-
 comments = []
 for text in list_sentences_train:
     comments.append(text_to_wordlist(text))
-    
-test_comments=[]
+
+test_comments = []
 for text in list_sentences_test:
     test_comments.append(text_to_wordlist(text))
 
@@ -267,37 +256,36 @@ for word, i in word_index.items():
 
 print('Null word embeddings: %d' % np.sum(np.sum(embedding_matrix, axis=1) == 0))
 
-
 ########################################
 ## sample train/validation data
 ########################################
 # np.random.seed(1234)
 perm = np.random.permutation(len(data))
-idx_train = perm[:int(len(data)*(1-VALIDATION_SPLIT))]
-idx_val = perm[int(len(data)*(1-VALIDATION_SPLIT)):]
+idx_train = perm[:int(len(data) * (1 - VALIDATION_SPLIT))]
+idx_val = perm[int(len(data) * (1 - VALIDATION_SPLIT)):]
 
-data_train=data[idx_train]
-labels_train=y[idx_train]
-print(data_train.shape,labels_train.shape)
+data_train = data[idx_train]
+labels_train = y[idx_train]
+print(data_train.shape, labels_train.shape)
 
-data_val=data[idx_val]
-labels_val=y[idx_val]
+data_val = data[idx_val]
+labels_val = y[idx_val]
 
-print(data_val.shape,labels_val.shape)
+print(data_val.shape, labels_val.shape)
 
 ########################################
 ## define the model structure
 ########################################
 embedding_layer = Embedding(nb_words,
-        EMBEDDING_DIM,
-        weights=[embedding_matrix],
-        input_length=MAX_SEQUENCE_LENGTH,
-        trainable=False)
+                            EMBEDDING_DIM,
+                            weights=[embedding_matrix],
+                            input_length=MAX_SEQUENCE_LENGTH,
+                            trainable=False)
 
-lstm_layer = LSTM(num_lstm, dropout=rate_drop_lstm, recurrent_dropout=rate_drop_lstm,return_sequences=True)
+lstm_layer = LSTM(num_lstm, dropout=rate_drop_lstm, recurrent_dropout=rate_drop_lstm, return_sequences=True)
 
 comment_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
-embedded_sequences= embedding_layer(comment_input)
+embedded_sequences = embedding_layer(comment_input)
 x = lstm_layer(embedded_sequences)
 x = Dropout(rate_drop_dense)(x)
 merged = Attention(MAX_SEQUENCE_LENGTH)(x)
@@ -310,24 +298,24 @@ preds = Dense(6, activation='sigmoid')(merged)
 ## train the model
 ########################################
 model = Model(inputs=[comment_input], \
-        outputs=preds)
+              outputs=preds)
 model.compile(loss='binary_crossentropy',
-        optimizer='rmsprop',
-        metrics=['accuracy'])
+              optimizer='rmsprop',
+              metrics=['accuracy'])
 print(model.summary())
 
-STAMP = 'simple_lstm_glove_vectors_%.2f_%.2f'%(rate_drop_lstm,rate_drop_dense)
+STAMP = 'simple_lstm_glove_vectors_%.2f_%.2f' % (rate_drop_lstm, rate_drop_dense)
 print(STAMP)
 
-early_stopping =EarlyStopping(monitor='val_loss', patience=5)
+early_stopping = EarlyStopping(monitor='val_loss', patience=5)
 bst_model_path = STAMP + '.h5'
 model_checkpoint = ModelCheckpoint(bst_model_path, save_best_only=True, save_weights_only=True)
 
 hist = model.fit(data_train, labels_train, \
-        validation_data=(data_val, labels_val), \
-        epochs=50, batch_size=256, shuffle=True, \
-         callbacks=[early_stopping, model_checkpoint])
-         
+                 validation_data=(data_val, labels_val), \
+                 epochs=50, batch_size=256, shuffle=True, \
+                 callbacks=[early_stopping, model_checkpoint])
+
 model.load_weights(bst_model_path)
 bst_val_score = min(hist.history['val_loss'])
 
@@ -341,4 +329,4 @@ y_test = model.predict([test_data], batch_size=1024, verbose=1)
 sample_submission = pd.read_csv("../input/sample_submission.csv")
 sample_submission[list_classes] = y_test
 
-sample_submission.to_csv('%.4f_'%(bst_val_score)+STAMP+'.csv', index=False)
+sample_submission.to_csv('%.4f_' % (bst_val_score) + STAMP + '.csv', index=False)
