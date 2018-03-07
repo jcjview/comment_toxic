484f583aae97c9487b06da2ac223fc5d16fa0a8a
chongjiu <chongjiu.jin@tinno.com>
2018-03-07 11:00:56 +0800

    add sh

--- final_craw_400/start.sh
+++ final_craw_400/start.sh
@@ -3,3 +3,5 @@ python keras_bgru_cv2_generate_fold10.py
 python keras_pooled_gru_cnn_cv_fold10.py
 python keras_ReLu_cv.py
 python keras_Attention_dropout.py
+python keras_capsule_fold10.py
+python keras_lstm_neptune_generate_cv_fold10.py
