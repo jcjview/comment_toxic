ee964e99158f61488fdbd4e10641ac5a67116fda
chongjiu <chongjiu.jin@tinno.com>
2018-03-15 16:20:42 +0800

    0.9863

--- submit_ensemble/20180312/pool/ensemble_pool.py
+++ submit_ensemble/20180312/pool/ensemble_pool.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 path = './'
 
-sup = pd.read_csv('0.9887_pooled_gru_cnn-Copy1_cv_fold10_craw_150.csv')#9854
+sup = pd.read_csv('0.9885_pooled_gru_cnn_cv_fold10_trans_craw_150_0314_9853.csv')#9853
 
 p1 = pd.read_csv('0.9886_pooled_gru_cnn_cv_fold10_craw_300.csv')#9855
 p2 = pd.read_csv('0.9884_pooled_gru_cnn_cv_fold10_0308_craw_150.csv')#9855
-p3 = pd.read_csv('0.9883_pooled_gru_cnn_cv_fold10glove_150_new_experiment_0312.csv')#9855
-p4 = pd.read_csv('0.9882_pooled_gru_cnn_cv_fold10_level_150_0313_9854.csv')#9854
+p3 = pd.read_csv('0.9891_pooled_gru_cnn_cv_fold10_800_9856.csv')#9856
+p4 = pd.read_csv('0.9885_two_rnn_cnn_cv_fold10.csv')#9855
 
 #
 #
@@ -34,4 +34,4 @@ blend[col] =0.15* minmax_scale(p1[col].values) + \
             0.35 *minmax_scale(p4[col].values)
 
 print('stay tight kaggler')
-blend.to_csv("ensemble_pool_gru_v1.csv", index=False,float_format='%.8f', compression='gzip')#0.9862
+blend.to_csv("ensemble_pool_gru_0315v3.csv.gz", index=False,float_format='%.8f', compression='gzip')#0.9863
